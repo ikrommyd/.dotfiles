c5411947f0e8b8fe2955aededa39ff03c58f2c3d
Iason Krommydas <iason.krom@gmail.com>
2025-05-05 00:36:12 -0500

    disable ligatures

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,14 @@
 local wezterm = require 'wezterm'
 
 return{
-  font = wezterm.font("MesloLGS Nerd Font Mono"),
+  -- font = wezterm.font("FiraCode Nerd Font Mono"),
   font_size = 13.0,
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 
   bold_brightens_ansi_colors = true,
 
   default_cursor_style = "BlinkingBlock",
-  cursor_blink_rate = 800,
+  cursor_blink_rate = 750,
   cursor_blink_ease_in = "Constant",
   cursor_blink_ease_out = "Constant",
 
